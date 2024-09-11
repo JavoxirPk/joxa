67d37939f40100219704626a351ce278bdd99072
JavoxirPk <javoxirpk@gmail.com>
2024-09-11 19:09:31 +0500

    Javoxir web site

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+        <p>All Right Resevered. <a href="https://www.youtube.com/@akhrorweb">Akhror Soliyev</a></p>
+      </div>
+    </div>
+  );
+}
+export default Footer;
